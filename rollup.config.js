be76783f29d52b97935426f2b4d3ce922e464cfa
zhaosaisai <zhaosaisai@u51.com>
2019-12-12 19:25:19 +0800

    chore: add build tools

---
+++ rollup.config.js
@@ -0,0 +1,64 @@
+import path from 'path'
+import ts from 'rollup-plugin-typescript2'
+import replace from '@rollup/plugin-replace'
+import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
+
+const resolve = (file) => path.resolve(__dirname, file)
+const isProduction = process.env.NODE_ENV === 'production'
+const simpleClone = obj => JSON.parse(JSON.stringify(obj))
+const addMinSuffix = str => str.replace(/\.js$/, '.min.js')
+
+const tsPlugin = ts({
+  tsconfig: resolve('tsconfig.json'),
+  cacheRoot: resolve('node_modules/.rts2_cache'),
+  tsconfigOverride: {
+    compilerOptions: {
+      declaration: isProduction
+    }
+  }
+})
+
+const defaultOutputs = ['umd', 'esm', 'cjs']
+
+const createOutputs = (ouputs) => {
+  const baseOutputs = Object.values(outputs)
+  return isProduction
+    ? baseOutputs.concat(simpleClone(baseOutputs).map(output => {
+      output.file = addMinSuffix(output.file)
+      return output
+    }))
+    : defaultOutputs.map(output => outputs[output])
+}
+
+const outputs = {
+  'esm': {
+    file: resolve('dist/angel.esm.js'),
+    format: 'es'
+  },
+  'cjs': {
+    file: resolve('dist/angel.cjs.js'),
+    format: 'cjs'
+  },
+  'umd': {
+    file: resolve('dist/angel.umd.js'),
+    format: 'umd'
+  }
+}
+
+module.exports = {
+  input: resolve('packages/index.ts'),
+  output: createOutputs(outputs),
+  plugins: [
+    tsPlugin,
+    replace({
+      __VERSION__: `${pkg.version}`
+    }),
+    isProduction ? terser({
+      include: /\.min\.js$/
+    }) : ''
+  ].filter(Boolean),
+  watch: {
+    exclude: 'node_modules/**'
+  }
+}
---
+++ verify-commit.js
@@ -0,0 +1,24 @@
+const chalk = require('chalk')
+const msgPath = process.env.GIT_PARAMS
+const msg = require('fs')
+  .readFileSync(msgPath, 'utf-8')
+  .trim()
+
+const commitRE = /^(revert: )?(feat|fix|docs|style|refactor|test|build|chore|types|normal|release)(\(.+\))?: .{1,50}/
+
+if (!commitRE.test(msg)) {
+  console.log()
+  console.error(
+    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(
+      `invalid commit message format.`
+    )}\n\n` +
+      chalk.red(
+        `  Proper commit message format is required for automated changelog generation. Examples:\n\n`
+      ) +
+      `    ${chalk.green(`feat(xhr): add 'fetch' option`)}\n` +
+      `    ${chalk.green(
+        `fix(performance): handle performance data error (close #28)`
+      )}\n\n`
+  )
+  process.exit(1)
+}

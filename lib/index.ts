df30cd2a4e2679e3009533d75df833874e91de8f
zhaosaisai <zhaosaisai@u51.com>
2020-01-22 16:28:17 +0800

    feat: 更改函数加载方式

--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,39 @@ import monitorError from './error'
 import monitorAjax from './ajax'
 import monitorResource from './resource'
 
-monitorPerformance()
-monitorError()
-monitorAjax()
-monitorResource()
+declare global {
+  interface Window {
+    __monitor__: any
+  }
+}
+
+function Monitor(config: any) {
+  this.config = config
+}
+
+Monitor.prototype.use = function(plugin: any) {
+  const installedPlugins = (this._installedPlugins || (this._installedPlugins = []))
+  if (installedPlugins.indexOf(plugin) > -1) {
+    return this
+  }
+
+  if (typeof plugin.install === 'function') {
+    plugin.install.call(null, this.config, this)
+  } else if (typeof plugin === 'function') {
+    plugin.call(null, this.config, this)
+  } else {
+    console.warn('plugin should be a function or an object with install method')
+  }
+
+  installedPlugins.push(plugin)
+  return this
+}
+
+export default Monitor
+window.__monitor__ = function(config: any) {
+  return new Monitor(config)
+    .use(monitorPerformance)
+    .use(monitorError)
+    .use(monitorAjax)
+    .use(monitorResource)
+}
